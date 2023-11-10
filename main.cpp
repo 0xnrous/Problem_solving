b0d7ac2d937dbdbaecb3ebbd9a511ee74d5fc49c
0xnrous <0xnrous@gmail.com>
2023-11-10 12:25:51 +0200

    first-update

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  PS
+//
+//  Created by 0xnrous on 09/11/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
