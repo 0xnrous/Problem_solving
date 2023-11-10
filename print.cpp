bb5bfc0f53b21f00cfa2485a50bba4f7237b34ca
0xnrous <0xnrous@gmail.com>
2023-11-10 13:25:26 +0200

    new_print_cpp

---
+++ print.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PS
+//
+//  Created by 0xnrous on 09/11/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Eldonia Magnona!\n";
+    std::cout <<    "Elhob lazeeeemmmmmmmmmm"  ;
+    return 0;
+}
